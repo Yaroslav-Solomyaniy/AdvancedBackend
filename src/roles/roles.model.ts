7d7d8b2026399bd743141eab97746b2e1a6cdc62
Yaroslav Solomyaniy <Soman74geeev@gmail.com>
2022-11-10 17:56:14 +0200

    add table users-roles

---
+++ src/roles/roles.model.ts
@@ -0,0 +1,39 @@
+import {
+  Model,
+  Column,
+  DataType,
+  Table,
+  BelongsToMany,
+} from 'sequelize-typescript';
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from '../users/users.model';
+import { UsersRoles } from './user-roles.model';
+
+interface RolesCreationAttrs {
+  value: string;
+  description: string;
+}
+
+@Table({ tableName: 'roles' })
+export class Role extends Model<Role, RolesCreationAttrs> {
+  @ApiProperty({ example: '1', description: 'Уникальный идентификатор' })
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+  @ApiProperty({
+    example: 'admin',
+    description: 'Уникальное значение роли пользователя',
+  })
+  @Column({ type: DataType.STRING, unique: true, allowNull: false })
+  value: string;
+  @ApiProperty({ example: 'Администратор', description: 'Описание роли' })
+  @Column({ type: DataType.STRING })
+  description: string;
+
+  @BelongsToMany(() => User, () => UsersRoles)
+  users: User[];
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,12 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './users.model';
+import { Role } from '../roles/roles.model';
+import { UsersRoles } from '../roles/user-roles.model';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [SequelizeModule.forFeature([User])],
+  imports: [SequelizeModule.forFeature([User, Role, UsersRoles])],
 })
 export class UsersModule {}
